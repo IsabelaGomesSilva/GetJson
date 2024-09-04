ee4a6951dc22f7d0d2644b7ac788be9f78a439e3
Isabela Silva <113267602+IsabelaGomesSilva@users.noreply.github.com>
2024-09-04 08:14:26 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,46 +1,42 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
 
-const handleRequest = (request, response) => {
-    switch (request.url) {
-        case '/':
-            response.writeHead(200, { 'Content-Type': 'text/plain' });
-            response.end('Hello World!');
-            break;
-        case '/users':
-            fs.readFile(path.join(__dirname, 'users.json'), (err, data) => {
-                if (err) {
-                    response.writeHead(500, { 'Content-Type': 'text/plain' });
-                    response.end('Internal Server Error');
-                    return;
-                }
-                response.writeHead(200, { 'Content-Type': 'application/json' });
-                response.end(data);
-            });
-            break;
-        case '/docs':
-            fs.readFile(path.join(__dirname, 'docs.json'), (err, data) => {
-                if (err) {
-                    response.writeHead(500, { 'Content-Type': 'text/plain' });
-                    response.end('Internal Server Error');
-                    return;
-                }
-                response.writeHead(200, { 'Content-Type': 'application/json' });
-                response.end(data);
-            });
-            break;
-        default:
-            response.writeHead(404, { 'Content-Type': 'text/plain' });
-            response.end('404 Not Found');
-            break;
-    }
-};
-
-const server = http.createServer(handleRequest);
-
+const app = express();
 const port = 3300;
 
-server.listen(port, () => {
-    console.log(`Server running at <http://localhost:${port}`);
-})
+const fileUsers = path.join(__dirname, './users.json');
+const fileDocuments = path.join(__dirname, './docs.json');
+
+app.use(express.json());
+
+
+app.get('/users',(req,res) => {
+    fs.readFile(fileUsers,'utf-8',(err,data) => {
+        if(err){
+            res.status(500).send('Erro arquivo de usuários');
+            return;
+        }
+        res.status(200).json(JSON.parse(data));
+    });
+});
+
+app.get('/docs',(req,res) => {
+    fs.readFile(fileDocuments,'utf-8',(err,data) => {
+        if(err){
+            res.status(500).send('Erro arquivo de documentos');
+            return;
+        }
+        res.status(200).json(JSON.parse(data));
+    });
+});
+
+
+app.use((req,res)=>{
+    res.status(404).send('404 page not found');
+});
+
+app.listen(port, () => {
+    console.log(`Server running http://localhost:${port}`);
+});
